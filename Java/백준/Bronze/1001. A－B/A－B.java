a895606973980c62d700f4b274ec92d9cdc4d36a
giwon-wg <giwon8766@naver.com>
2025-03-04 13:55:26 +0900

    [Bronze V] Title: A-B, Time: 172 ms, Memory: 17728 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner ab = new Scanner(System.in);
+        a = ab.nextInt();
+        b = ab.nextInt();
+
+        System.out.println( a - b );
+
+    }
+}
